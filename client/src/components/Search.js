62f7930a5282de21fff5daa006f294616ab7950b
jessicagallagher <jessicalyngallagher@me.com>
2022-06-30 15:14:09 -0400

    Revert "finish algolia integration for create"
    This reverts commit a0f87ddde765c6f2e1fa36f2c1c0935eabff4fdd.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,56 +1,44 @@
-import { useState } from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import {
   InstantSearch,
   SearchBox,
   Hits,
+  Highlight,
   Stats,
+  SortBy,
   Pagination,
   RefinementList,
   ClearRefinements,
+  Configure
 } from 'react-instantsearch-dom';
 import OneRecordWrapper from '../assets/wrappers/OneRecordWrapper';
 import { OneSandbox } from '../components'
 
 export default function Search() {
-  const [clicked, setClicked] = useState(false);
-
   const searchClient = algoliasearch(
-    process.env.REACT_APP_ALGOLIA_ID,
-    process.env.REACT_APP_SEARCH_API
+    'R09QEE164L',
+    '7381ee2bbf457ce7bf24686e1dd38bc2'
   );
-  const index = process.env.REACT_APP_ALGOLIA_SANDBOX_INDEX;
-
-  const handleToggle = (e) => {
-    e.preventDefault();
-
-    setClicked(!clicked)
-  }
 
   return (
     <>
-      <div className='search-container'>
-        <InstantSearch searchClient={searchClient} indexName={index}>
-          <Header />
-          <h5 className='r2b-red'>🌎 Filter by state</h5>
-          <RefinementList attribute='state' />
-          <ClearRefinements />
-          <Content />
-        </InstantSearch>
-      </div>
+      <h5 className='r2b-blue space'>Search</h5>
+      <InstantSearch searchClient={searchClient} indexName='sandbox'>
+        <Configure hitsPerPage={10} />
+        <Header />
+        <h5 className='r2b-blue'>Filter by state</h5>
+        <RefinementList attribute='state' />
+        <RefinementList attribute='lastName' />
+        <ClearRefinements />
+        <Content />
+      </InstantSearch>
     </>
   );
 }
 
 const Header = () => (
-  <div className='search-container-child'>
-    <h4 className='title'>🕵️ WHAT ARE YOU LOOKING FOR?</h4>
-    <SearchBox
-      translations={{ placeholder: 'Enter first name, last name, or email' }}
-      showLoadingIndicator
-    />
-  </div>
-);
+  <SearchBox translations={{ placeholder: 'Enter first name, last name, or email' }}/>
+)
 
 const Hit = ({ hit, updateSandbox }) => (
   <OneSandbox
@@ -71,6 +59,6 @@ const Content = () => (
   <div>
     <Stats />
     <Hits hitComponent={Hit} />
-    <Pagination totalPages={2} />
+    <Pagination />
   </div>
 );
