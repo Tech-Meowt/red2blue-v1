c1d19ac2bc7d8bd981810b5d4e3dda650b9a7d04
jessicagallagher <jessicalyngallagher@me.com>
2022-08-12 14:30:22 -0400

    add pagination

--- client/src/components/PoliticalTable.js
+++ client/src/components/PoliticalTable.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react'
-import { useTable, useSortBy, useFilters } from 'react-table';
+import { useTable, useSortBy, useFilters, usePagination } from 'react-table';
 import { Filter, DefaultColumnFilter } from './Filter'
-import { Table } from 'reactstrap'
+import { Table, Row, Col, Button, Input } from 'reactstrap'
 
 const PoliticalTable = ({ columns, data }) => {
   const defaultColumn = useMemo(
@@ -16,32 +16,45 @@ const PoliticalTable = ({ columns, data }) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
     prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
   } = useTable(
     {
-    columns,
+      columns,
       data,
-    defaultColumn: { Filter: DefaultColumnFilter }
+      defaultColumn: { Filter: DefaultColumnFilter },
+      initialState: { pageIndex: 0, pageSize: 10 }
     },
     useFilters,
     useSortBy,
-    )
+    usePagination
+  );
   
   const generateSortingIndicator = (column) => {
     return column.isSorted ? (column.isSortedDesc ? '  🔽' : '  🔼') : '';
   };
 
+  const onChangeInSelect = (e) => {
+    setPageSize(Number(e.target.value))
+  }
+
+  const onChangeInInput = (e) => {
+    const page = e.target.value ? Number(e.target.value) - 1 : gotoPage(page)
+  }
+
   return (
     <>
       <div className='overflow-scroll overflow-visible container-sm'>
-        <Table
-          bordered
-          hover
-          striped
-          size='sm'
-          {...getTableProps()}
-        >
+        <Table bordered hover striped size='sm' {...getTableProps()}>
           <thead>
             {headerGroups.map((headerGroup) => (
               <tr {...headerGroup.getHeaderGroupProps()}>
@@ -62,7 +75,7 @@ const PoliticalTable = ({ columns, data }) => {
           </thead>
 
           <tbody {...getTableBodyProps()} className='rows'>
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
@@ -78,6 +91,68 @@ const PoliticalTable = ({ columns, data }) => {
             })}
           </tbody>
         </Table>
+        <Row style={{ maxWidth: 1000, margin: '0 auto', textAlign: 'center' }}>
+          <Col md={3}>
+            <Button
+              color='danger'
+              onClick={() => gotoPage(0)}
+              disabled={!canPreviousPage}
+            >
+              {'<<'}
+            </Button>
+            <Button
+              color='danger'
+              onClick={previousPage}
+              disabled={!canPreviousPage}
+            >
+              {'<'}
+            </Button>
+          </Col>
+          <Col md={2} style={{ marginTop: 7 }}>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>
+          </Col>
+          <Col md={2}>
+            <Input
+              type='number'
+              min={1}
+              style={{ width: 70 }}
+              max={pageOptions.length}
+              defaultValue={pageIndex + 1}
+              onChange={onChangeInInput}
+            />
+          </Col>
+          <Col md={2}>
+            <Input
+              type='select'
+              id='custom-select'
+              bsSize='sm'
+              value={pageSize}
+              onChange={onChangeInSelect}
+            >
+              >
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </Input>
+          </Col>
+          <Col md={3}>
+            <Button color='danger' onClick={nextPage} disabled={!canNextPage}>
+              {'>'}
+            </Button>
+            <Button
+              color='danger'
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+            >
+              {'>>'}
+            </Button>
+          </Col>
+        </Row>
       </div>
     </>
   );
