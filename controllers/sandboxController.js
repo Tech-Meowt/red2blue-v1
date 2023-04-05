2f08a08df4e824f4f2d426c69d043f55f1334fab
jessicagallagher <jessicalyngallagher@me.com>
2023-04-05 18:01:18 -0400

    migrate sandbox db

--- controllers/sandboxController.js
+++ controllers/sandboxController.js
@@ -1,74 +1,65 @@
-import prisma from '../lib/prisma.js';
+import Sandbox from '../models/Sandbox.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import mongoose from 'mongoose';
 
 const create = async (req, res) => {
-  const { firstName, lastName, email, state, phone, interests, createdAt } =
-    req.body;
-
-  const sandbox = await prisma.sandbox.create({
-    data: {
-      firstName,
-      lastName,
-      email,
-      state,
-      phone,
-      interests,
-      createdAt,
-    },
-  });
-  res.status(200).json({ sandbox });
-};
-
-
-const getAll = async (req, res) => {
-  const sandbox = await prisma.sandbox.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    }
-  });
-  res.status(200).json({ sandbox });
-};
-
-const updateSandbox = async (req, res) => {
-  const { id } = req.params;
   const {
     firstName,
     lastName,
     email,
-    state,
-    phone,
-    interests,
+    phoneNumber,
+    favoriteColor,
+    favoriteMovie,
   } = req.body;
 
-  const sandbox = await prisma.sandbox.update({
-    where: {
-      id,
-    },
-    data: {
-      id,
-      firstName,
-      lastName,
-      email,
-      state,
-      phone,
-      interests,
-    },
-  });
-  res.status(200).json({ sandbox });
+  const sandbox = await Sandbox.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ sandbox });
 };
 
+const getAll = async (req, res) => {
+  const sandbox = await Sandbox.find({}).sort({ updatedAt: 'desc' });
 
+  res.status(StatusCodes.OK).json({ sandbox });
+};
+
+const updateSandbox = async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    favoriteColor,
+    favoriteMovie,
+  } = req.body;
+
+  const sandbox = await Sandbox.findOne({ _id: req.params.id }, req.body);
+
+  sandbox.firstName = firstName;
+  sandbox.lastName = lastName;
+  sandbox.email = email;
+  sandbox.phoneNumber = phoneNumber;
+  sandbox.favoriteColor = favoriteColor;
+  sandbox.favoriteMovie = favoriteMovie;
+
+  await sandbox.save();
+
+  res.status(StatusCodes.OK).json({ sandbox });
+};
 
 const deleteSandbox = async (req, res) => {
-  const { id } = req.params;
+  const { id: sandboxId } = req.params;
 
-  const sandbox = await prisma.sandbox.delete({
-    where: {
-      id,
-    },
-  });
-  res.status(200).json({});
+  const sandbox = await Sandbox.findOne({ _id: sandboxId });
+
+  if (!sandbox) {
+    throw new NotFoundError(`No record with id: ${sandboxId}`);
+  }
+
+  await sandbox.remove();
+
+  res.status(StatusCodes.OK).json({ msg: `Record deleted` });
 };
 
 export { create, getAll, updateSandbox, deleteSandbox };
